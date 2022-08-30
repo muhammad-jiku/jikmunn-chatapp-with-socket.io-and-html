4f283b013b63dd9701ba04719ee05c1df345478c
muhammad-jiku <muhammadjiku364@gmail.com>
2022-08-31 00:23:08 +0600

    socket configuration is added

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const { Server } = require('socket.io');
 const port = process.env.PORT || 5000;
 const expressServer = http.createServer(app);
+
+const io = new Server(expressServer);
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   //   res.send('Helllo there!!');
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('user connected');
 });
 
 expressServer.listen(port, () => {
